8d6e48a6f5bf388106f3b5d8d1d8413faf24b665
Bill Edwards <wae20@cantab.net>
2015-02-11 19:15:28 +0000

    Added sign-up (with password hashing) and delete user functionalities.

--- models/user_model.js
+++ models/user_model.js
@@ -34,6 +34,22 @@ var userSchema = mongoose.Schema({
 	}
 });
 
+// Hash password. 
+userSchema.pre('save',function(next){
+
+	var user = this; 
+
+	// If password is not being updated, skip to next middleware. 
+	if (!user.isModified('password')) next(); // Ben does {return next()} !! 
+
+	// Apply cryptographic hash function to password. 
+	bcrypt.hash(user.password, 10, function(err, hash){
+		if (err) next(err); // Ben does {return next()} !! 
+		user.password = hash; 
+		next(); 
+	});
+});
+
 // Compare supplied cleartext and stored hashed passwords
 // Arguably the body of this could just be included directly in the login_route code. 
 // I suppose it means we (1) only require bcrypt once; (2) can potentially reuse elswhere. 
@@ -51,6 +67,15 @@ userSchema.methods.dataForClient = function() {
 	delete userClone.loginAttempts;
 	delete userClone.timeout; 
 	return userClone; 
+};
+
+// Check availability of username. 
+userSchema.statics.isUsernameTaken = function(suppliedUsername, callback) {
+	this.findOne({username: suppliedUsername}, function(err, user){
+		if (err) return callback(err); 
+		if (!user) return callback(null, false); 
+		return callback(null, true); 
+	});
 };
 
 
--- routes/user_route.js
+++ routes/user_route.js
@@ -4,11 +4,50 @@
 
 var express = require('express'),
 	path = require('path'),
+	bodyParser = require('body-parser'),
 	gatekeeper = require(path.join(__dirname, '../utilities/gatekeeper')),
+	validator = require(path.join(__dirname, '../utilities/validator')),
 	User = require(path.join(__dirname, '../models/user_model.js')),
 	AppError = require(path.join(__dirname, '../app_error.js')); 
 
 var router = express.Router(); 
+
+router.route('/user/new')
+
+	// Create new user
+	.post(bodyParser.json(), validator("user", ["username", "password"]), function(req, res, next){
+
+		// Check whether username is available. 
+		User.isUsernameTaken(req.body.username, function(err, taken){
+			if (err) return next(err); 
+			// Username already in use. 
+			if (taken) return next (new AppError(400, "Username in use"));
+			
+			// Username is available... we proceed. 
+			if (!taken) {
+				var user = User();
+				user.username = req.body.username; 
+				user.password = req.body.password; 
+				user.createDate = Date.now(); 
+				user.lastLogin = Date.now(); 
+				user.loginAttempts = 0; 
+				user.timeout = 0; 
+
+				// Save new user's details in DB. 
+				user.save(function(err){
+					if (err) res.send(err);
+
+					// Set up new session
+					req.session.regenerate(function(){
+						req.session.userId = user._id; 
+
+						// Return data to client. 
+						res.json(user.dataForClient()); 
+					});
+				});
+			}
+		});
+	}); 
 
 router.route('/user/me')
 
@@ -20,7 +59,22 @@ router.route('/user/me')
 			res.json(user.dataForClient()); 
 		});
 
-	});
+	})
+
+	// Edit own user details. 
+	.put(gatekeeper, validator("user", []), function(req, res, next){
+
+	})
+
+	// Delete own user details. 
+	.delete(gatekeeper, function(req, res, next){
+		User.remove({_id:req.session.userId}, function(err){
+			if (err) return next(err); 
+			req.session.destroy(function(){
+				res.json({}); 
+			});
+		});
+	}); 
 
 router.route('/user/:user_id')
 
