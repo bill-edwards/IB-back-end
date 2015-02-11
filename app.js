7de552b7598616fcabb5ea6f5557478b255da1ec
Bill Edwards <wae20@cantab.net>
2015-02-11 15:19:52 +0000

    Ready to merge into master

--- app.js
+++ app.js
@@ -20,10 +20,6 @@ mongoose.connect('mongodb://localhost:27017/icebreakr');
 app.use(express.static(path.join(__dirname, '../public'), { maxAge: '28d' }));
 
 
-// Route for testing. 
-// REMOVE WHEN MERGING INTO MASTER. 
-app.use('/api', require(__dirname + '/dev/test_route.js'));
-
 // Homepage
 app.route('*').get(function(req, res, next){
 	res.send("It's working!");
